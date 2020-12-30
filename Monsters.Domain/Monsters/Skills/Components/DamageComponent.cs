d3a077968985a5d98bd70b05deb41e1f2d051e16
WaveHack <email@wavehack.net>
2020-12-30 21:21:12 +0100

    Remove comments

--- Monsters.Domain/Monsters/Skills/Components/DamageComponent.cs
+++ Monsters.Domain/Monsters/Skills/Components/DamageComponent.cs
@@ -3,8 +3,5 @@
     public class DamageComponent : Component
     {
         public int DamageMultiplier { get; set; }
-
-        // single hit vs multi hit
-        // side effects (based on chance%, like debuffs, additional hits etc)? maybe additional component on the skill?
     }
 }
