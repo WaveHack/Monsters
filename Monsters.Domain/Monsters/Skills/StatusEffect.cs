4f9b88fef433b600d03cd51641da958a943ba4fa
WaveHack <email@wavehack.net>
2020-12-31 01:16:51 +0100

    Add a bunch of WIP status effects

--- Monsters.Domain/Monsters/Skills/StatusEffect.cs
+++ Monsters.Domain/Monsters/Skills/StatusEffect.cs
@@ -2,6 +2,39 @@
 {
     public enum StatusEffect
     {
+        AttackBoost, // +50%
+        AttackBreak, // -50%
+        DefenseBoost, // +50%
+        DefenseBreak, // -70%
+        SpeedBoost, // +30%
+        SpeedBreak, // -30%
+        CriticalRateBoost, // +30%
+
+        Immunity,
+        BlockBeneficialEffects,
+
         Poison,
+        ContinuousDamage, // 5%
+
+        Invincibility,
+        Defend,
+        Endure,
+        Revenge, // 50%
+        Recovery, // 15%
+        Counter, // 100%
+        ProtectSoul,
+        Reflect, // 33%
+        CriticalRateResist, // -50%
+        Shield,
+        Glancing, // +50%
+        Brand, // +25%
+        Unrecoverable,
+        Taunt,
+        Oblivion,
+        Bomb,
+        Freeze,
+        Stun,
+        Sleep,
+        Silence,
     }
 }
